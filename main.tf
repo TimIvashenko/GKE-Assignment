97b5dc4ff0ad16544d99d82170866259cf4b1db5
Tim Ivashenko <146694293+TimIvashenko@users.noreply.github.com>
2024-01-11 11:51:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
-
+// internal IP allocation
 resource "google_compute_address" "mongodb_node_ip" {
   count        = 3
   name         = "mongodb-node-ip-${count.index}"
@@ -13,8 +13,7 @@ resource "google_compute_address" "mongodb_node_ip" {
   address      = "10.0.1.${count.index + 2}"
   region       = var.region
 }
-
-// Creation of the 3 Compute Engine instances for the MongoDB cluster.
+// Creation of the 3 Compute Engine instances for the MongoDB cluster using count in each zone, can be in one zone for it to be less expensive but one point of failure
 resource "google_compute_instance" "mongo_node" {
   count        = 3
   name         = "mongodb-node${count.index + 1}"
@@ -30,10 +29,10 @@ resource "google_compute_instance" "mongo_node" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      size  = 10
+      size  = 10 # default persistent disk, ssd if read/write speed is important
     }
   }
-
+// Creation of my-vpc and private subnet mongodb-subnet
   network_interface {
     network    = var.network
     subnetwork = var.subnet
@@ -45,13 +44,13 @@ resource "google_compute_instance" "mongo_node" {
     "enable-oslogin" = "TRUE"
   }
 
-  tags = ["allow-mongodb"]
+  tags = ["allow-mongodb", "nodeapp"]
 
   depends_on = [
     google_compute_network.my_vpc,
     google_compute_subnetwork.subnet_mongodb
   ]
-
+// startup script to configure mongodb instances and intiate a replica set
 metadata_startup_script = file("/home/tim/Desktop/Home_Directory/testmongodb/terraform/script/setup_mongodb.sh")
 
 }
