9724d3d6021f3e9c90bd3f636888c56c56fdce80
Tim Ivashenko <146694293+TimIvashenko@users.noreply.github.com>
2024-01-11 11:52:37 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,8 @@ output "mongodb_instance_details" {
       instance.name => google_compute_address.mongodb_node_ip[idx].address 
   }
 }
+
+output "instance_tags" {
+  value = { for instance in google_compute_instance.mongo_node : instance.name => instance.tags }
+  description = "The tags of each MongoDB instance"
+}
