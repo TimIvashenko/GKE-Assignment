2dd5a55c6acd87d9345cbb1b5871de5030fdbcf7
Tim Ivashenko <146694293+TimIvashenko@users.noreply.github.com>
2024-01-11 11:52:19 +0200

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,4 +1,4 @@
-resource "google_compute_firewall" "ssh_firewall" {
+resource "google_compute_firewall" "ssh_firewall" { # ssh for testing
   name    = "ssh-firewall"
   network = google_compute_network.my_vpc.name
 
@@ -7,11 +7,11 @@ resource "google_compute_firewall" "ssh_firewall" {
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["allow-mongodb"]
+  source_ranges = ["0.0.0.0/0"] # my IP or VPN is more secure
+  target_tags   = ["allow-mongodb", "nodeapp"]
 }
 
-resource "google_compute_firewall" "mongodb_firewall" {
+resource "google_compute_firewall" "mongodb_firewall" { # mongodb port
   name    = "mongodb-firewall"
   network = google_compute_network.my_vpc.name
 
@@ -20,11 +20,11 @@ resource "google_compute_firewall" "mongodb_firewall" {
     ports    = ["27017"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["allow-mongodb"]
+  source_ranges = ["0.0.0.0/0"] # instances IP more secure
+  target_tags   = ["allow-mongodb", "nodeapp"]
 }
 
-resource "google_compute_firewall" "nodeapp_firewall" {
+resource "google_compute_firewall" "nodeapp_firewall" { # node app port
   name    = "nodeapp-firewall"
   network = google_compute_network.my_vpc.name
 
@@ -33,6 +33,32 @@ resource "google_compute_firewall" "nodeapp_firewall" {
     ports    = ["3000", "80", "443"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["nodeapp"]
+  source_ranges = ["0.0.0.0/0"] # instances IP more secure
+  target_tags   = ["allow-mongodb", "nodeapp"]
+}
+
+resource "google_compute_firewall" "all_pods_and_master_ipv4_cidrs" { # for pods and master for gke
+  name    = "all-pods-and-master-ipv4-cidrs"
+  network = google_compute_network.my_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["10.0.0.0/8", "172.16.2.0/28"] # range of ports
+}
+
+resource "google_compute_firewall" "allow_nginx_ingress" { # allow nginx ingress
+  name    = "allow-nginx-ingress"
+  network = google_compute_network.my_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  direction   = "INGRESS"
+  target_tags = ["nodeapp"]
 }
